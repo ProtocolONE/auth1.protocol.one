a194bc81925c315388afbcb48c814d7c47eac012
Nikolay Bondarenko <misterionkell@gmail.com>
2019-01-29 16:32:05 +0300

    Migrate logout manager and router to zap

--- pkg/manager/logout.go
+++ pkg/manager/logout.go
@@ -3,46 +3,63 @@ package manager
 import (
 	"auth-one-api/pkg/database"
 	"auth-one-api/pkg/models"
-	"fmt"
 	"github.com/labstack/echo"
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
 )
 
-type LogoutManager Config
+type LogoutManager struct {
+	logger     *zap.Logger
+	appService *models.ApplicationService
+}
+
+func NewLogoutManager(logger *zap.Logger, db *database.Handler) *LogoutManager {
+	m := &LogoutManager{
+		logger:     logger,
+		appService: models.NewApplicationService(db),
+	}
+
+	return m
+}
 
 func (m *LogoutManager) Logout(r *echo.Response, form *models.LogoutForm) (error models.ErrorInterface) {
 	if form.RedirectUri == `bad_redirect_uri` {
 		return &models.CommonError{Message: models.ErrorRedirectUriIncorrect}
 	}
 
-	as := models.NewApplicationService(m.Database)
-	a, err := as.Get(bson.ObjectIdHex(form.ClientId))
+	a, err := m.appService.Get(bson.ObjectIdHex(form.ClientId))
 	if err != nil {
+		m.logger.Error(
+			"Unable to get application",
+			zap.String("clientId", form.ClientId),
+			zap.Error(err),
+		)
+
 		return &models.CommonError{Code: `client_id`, Message: models.ErrorClientIdIncorrect}
 	}
 
-	cs, err := as.LoadSessionSettings()
+	cs, err := m.appService.LoadSessionSettings()
 	if err != nil {
-		m.Logger.Warning(fmt.Sprintf("Unable to load session settings an application [%s] with error: %s", a.ID, err.Error()))
+		m.logger.Error(
+			"Unable to load session settings an application",
+			zap.String("appId", a.ID.String()),
+			zap.Error(err),
+		)
+
 		return &models.CommonError{Code: `common`, Message: models.ErrorCreateCookie}
 	}
 	c := models.NewCookie(a, &models.User{}).Clear(cs)
 	if err != nil {
-		m.Logger.Warning(fmt.Sprintf("Unable to clear cookie an application [%s] with error: %s", a.ID, err.Error()))
+		m.logger.Error(
+			"Unable to clear cookie an application",
+			zap.String("appId", a.ID.String()),
+			zap.Error(err),
+		)
+
 		return &models.CommonError{Code: `common`, Message: models.ErrorCreateCookie}
 	}
+
 	http.SetCookie(r, c)
-
 	return nil
-}
-
-func InitLogoutManager(logger *logrus.Entry, db *database.Handler) LogoutManager {
-	m := LogoutManager{
-		Database: db,
-		Logger:   logger,
-	}
-
-	return m
 }
--- pkg/route/logout.go
+++ pkg/route/logout.go
@@ -6,16 +6,19 @@ import (
 	"auth-one-api/pkg/models"
 	"fmt"
 	"github.com/labstack/echo"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 type Logout struct {
-	Manager manager.LogoutManager
+	Manager *manager.LogoutManager
+	logger  *zap.Logger
 }
 
 func LogoutInit(cfg Config) error {
 	route := &Logout{
-		Manager: manager.InitLogoutManager(cfg.Logger, cfg.Database),
+		Manager: manager.NewLogoutManager(cfg.Logger, cfg.Database),
+		logger:  cfg.Logger,
 	}
 
 	cfg.Echo.GET("/logout", route.Logout)
@@ -27,6 +30,8 @@ func (l *Logout) Logout(ctx echo.Context) error {
 	form := new(models.LogoutForm)
 
 	if err := ctx.Bind(form); err != nil {
+		l.logger.Error("Logout bind form failed", zap.Error(err))
+
 		return helper.NewErrorResponse(
 			ctx,
 			http.StatusBadRequest,
@@ -36,6 +41,8 @@ func (l *Logout) Logout(ctx echo.Context) error {
 	}
 
 	if err := ctx.Validate(form); err != nil {
+		l.logger.Error("Logout validate form failed", zap.Error(err))
+
 		return helper.NewErrorResponse(
 			ctx,
 			http.StatusBadRequest,
