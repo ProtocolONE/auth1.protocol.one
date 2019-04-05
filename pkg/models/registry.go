a14f93db00a857e309b70fbee0b331de940d07a7
Nikolay Bondarenko <misterionkell@gmail.com>
2019-04-05 15:25:43 +0300

    Add Registry object to manage same deps across managers

---
+++ pkg/models/registry.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"github.com/ProtocolONE/auth1.protocol.one/pkg/persist"
+	"github.com/ProtocolONE/mfa-service/pkg/proto"
+	"github.com/globalsign/mgo"
+	"github.com/ory/hydra/sdk/go/hydra"
+)
+
+type InternalRegistry interface {
+	Watcher() persist.Watcher
+	MgoSession() *mgo.Session
+	HydraSDK() *hydra.CodeGenSDK
+	MfaService() proto.MfaService
+	ApplicationService() *ApplicationService
+	OneTimeTokenService() *OneTimeTokenService
+}
