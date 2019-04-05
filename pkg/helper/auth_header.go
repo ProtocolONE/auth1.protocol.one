2ed36776a7789ce2141f9ccb47c266f80d6dc9f2
Nikolay Bondarenko <misterionkell@gmail.com>
2019-04-05 15:24:31 +0300

    Remove unused code from helpers (now same code used from Hydra sdk)

--- pkg/helper/auth_header.go
+++ pkg/helper/auth_header.go
@@ -27,20 +27,5 @@ func GetTokenFromAuthHeader(as *models.ApplicationService, headers http.Header) 
 		return nil, errors.New(`invalid client id`)
 	}
 
-	ats, err := as.LoadAuthTokenSettings()
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to load token settings")
-	}
-
-	jts := models.NewJwtTokenService(ats)
-	c, err := jts.Decode(s[1])
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to load jwt settings")
-	}
-
-	if err = c.Valid(); err != nil {
-		return nil, errors.Wrap(err, "token is invalid")
-	}
-
-	return c, nil
+	return nil, nil
 }
