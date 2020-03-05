2a883aa997ebf724aa626c8619d69d417f0bbb0c
Aleksandr Barsukov <aleksandr.barsukov@protocol.one>
2020-03-05 14:16:17 +0300

    remove unused collections from mongo

---
+++ pkg/database/migrations/20200305_01_remove_log_tables.go
@@ -0,0 +1,25 @@
+package migrations
+
+import (
+	"github.com/globalsign/mgo"
+	"github.com/xakep666/mongo-migrate"
+)
+
+func init() {
+	err := migrate.Register(
+		func(db *mgo.Database) error {
+			// removed (normalization in auth_log not needed)
+			 db.C("user_agent").DropCollection()
+			 db.C("user_ip").DropCollection()
+
+			return nil
+		},
+		func(db *mgo.Database) error {
+					return nil
+		},
+	)
+
+	if err != nil {
+		return
+	}
+}
